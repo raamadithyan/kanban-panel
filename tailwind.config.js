6e22c479ecc0bbdfe3932442effccce20566874c
raamadithyan <chitha.don@gmail.com>
2023-10-12 00:04:05 +0530

    branch check

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,9 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    fontFamily: {
+      'sans': ['Roboto', 'Arial', 'sans-serif'],
+    },
     extend: {},
   },
   plugins: [],
